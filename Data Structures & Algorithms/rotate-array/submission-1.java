6ccfb58836f89c09bd060038200cf4b99d1ffd4a
Sravan1012 <sravankolluru10@gmail.com>
2026-06-05 15:33:40 +0530

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        if(k == 0) return;
+        int[] temp = new int[k];
+        for(int i = 0; i < k; i++) temp[i] = nums[n - 1 - i];
+        for(int i = n - 1; i >= k; i--) nums[i] = nums[i - k];
+        for(int i = 0; i < k; i++) {
+            nums[i] = temp[k - i - 1];
+        }
+    }
+}
